49692089a6d14791a896bf22b253557acf0e8b7d
Kouki Shimoya <shimoyameiji@gmail.com>
2019-10-20 14:44:28 +0900

    ひさとくんの画像を追加

---
+++ FindChild/Assets/C#/General/SingletonMonoBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var t = typeof(T);
+
+                instance = (T) FindObjectOfType(t);
+                if (instance == null)
+                {
+                    Debug.LogError(t + " をアタッチしているGameObjectはありません");
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        // 他のゲームオブジェクトにアタッチされているか調べる
+        // アタッチされている場合は破棄する。
+        CheckInstance();
+    }
+
+    protected bool CheckInstance()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+            return true;
+        }
+
+        if (Instance == this)
+        {
+            return true;
+        }
+
+        Debug.Log("Accurate. " + GetType().Name + "型のオブジェクトは既に存在します。古いオブジェクトを削除します");
+        Destroy(gameObject);
+        return false;
+    }
+}
---
+++ FindChild/Assets/C#/ParentMove.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentMove : MonoBehaviour
+{
+    private bool isReachTarget;
+    private Vector2 targetPosition;
+
+    private const int xMin = -960;
+    private const int xMax = 960;
+    private const int yMin = -540;
+    private const int yMax = 540;
+    private bool canMove;
+
+    [SerializeField] private float defaultSpeed = 1f;
+
+    private void Start()
+    {
+        canMove = true;
+        isReachTarget = false;
+        DecideTargetPosition();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+        
+        DecideTargetPosition();
+
+        this.gameObject.transform.position =
+            Vector2.MoveTowards(this.gameObject.transform.position, targetPosition, defaultSpeed);
+
+        if (Vector2.Distance(this.gameObject.transform.position, targetPosition) < 1f)
+        {
+            isReachTarget = true;
+        }
+    }
+
+    private void DecideTargetPosition()
+    {
+        if (!isReachTarget)
+        {
+            return;
+        }
+        
+        targetPosition = new Vector2(UnityEngine.Random.Range(xMin, xMax), UnityEngine.Random.Range(yMin, yMax));
+        isReachTarget = false;
+    }
+}
